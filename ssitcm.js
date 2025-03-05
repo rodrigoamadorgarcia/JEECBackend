c30219958752f92a4be42dbd85e6170113749199
Rodrigo Amador <rodrigoalbertoamador@gmail.com>
2025-03-04 21:22:51 -0600

    Actualización en el .js

--- ssitcm.js
+++ ssitcm.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const dbConfig = {
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 };
+
 
 const db = mysql.createPool(dbConfig);
 
